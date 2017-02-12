d01d2955293e5e74f18eebd740c5fb356d67051a
Ben Hall <benhall22@hotmail.co.uk>
2017-02-12 21:20:59 +0000

    feat:rayMarchingBlobs shiny, different colors, camera looking around

--- glsl/raymarchingBlobs.glsl
+++ glsl/raymarchingBlobs.glsl
@@ -7,12 +7,12 @@ uniform vec2 u_resolution;
 
 const float eps = 0.005;
 
-const int maxIterations = 64;
-const float stepScale = 0.75;
-const float stopThreshold = 0.005;
-
 float sphere(in vec3 p, float radius) {
 	return length(p) - radius;
+}
+
+float sinusoidalPlasma(in vec3 p){
+  return sin(p.x+u_time*2.)*cos(p.y+u_time*2.1)*sin(p.z+u_time*2.3) + 0.25*sin(p.x*2.)*cos(p.y*2.)*sin(p.z*2.);
 }
 
 float sinusoidBumps(in vec3 p){
@@ -25,22 +25,19 @@ float scene(in vec3 p) {
 }
 
 vec3 getNormal(in vec3 p) {
-  float ref = scene(p);
-	return normalize(vec3(
-		scene(vec3(p.x+eps,p.y,p.z))-ref,
-		scene(vec3(p.x,p.y+eps,p.z))-ref,
-		scene(vec3(p.x,p.y,p.z+eps))-ref
-	));
+  vec2 e = vec2(-.5 * eps, .5 * eps);
+  return normalize(e.yxx*scene(p+e.yxx)+e.xxy*scene(p+e.xxy)+e.xyx*scene(p+e.xyx)+e.yyy*scene(p+e.yyy));
 }
 
 float rayMarching(vec3 origin, vec3 dir, float start, float end) {
+  const float stopThreshold = 0.005;
 	float sceneDist = 1e4;
 	float rayDepth = start;
 
-	for (int i = 0; i < maxIterations; i++) {
+	for (int i = 0; i < 64; i++) {
 		sceneDist = scene(origin + dir * rayDepth);
 		if ((sceneDist < stopThreshold) || (rayDepth >= end)) break;
-		rayDepth += sceneDist * stepScale;
+		rayDepth += sceneDist * .8;
 	}
 
 	if (sceneDist >= stopThreshold) rayDepth = end;
@@ -49,11 +46,50 @@ float rayMarching(vec3 origin, vec3 dir, float start, float end) {
 	return rayDepth;
 }
 
+vec3 lighting(vec3 surfacePosition, vec3 camPos, int reflectionPass){
+    vec3 sceneColor = vec3(0.);
+    vec3 voxPos = mod(surfacePosition*.25, 1.);
+    vec3 objColor = vec3(.3);
+
+    if ((voxPos.x<0.5)&&(voxPos.y>=0.5)&&(voxPos.z<0.5)) objColor = vec3(voxPos.x,voxPos.z,.5) * .33;
+    else if ((voxPos.x>=0.5)&&(voxPos.y<0.5)&&(voxPos.z>=0.5)) objColor = vec3(voxPos.y,.5,voxPos.z) * .33;
+
+    float bumps = sinusoidBumps(surfacePosition);
+    objColor = clamp(objColor * .8 - vec3(.1, .4, .5) * bumps, 0., 1.);
+
+    float fakeShadowMovement =  sinusoidalPlasma(surfacePosition*8.);
+    objColor = clamp(objColor*(0.75-0.25*fakeShadowMovement), 0.0, 1.0);
+
+    vec3 surfNormal = getNormal(surfacePosition);
+
+    vec3 lightPosition = vec3(0., 1., 0. + u_time);
+    vec3 lightDirection = lightPosition - surfacePosition;
+    vec3 lightColor = vec3(1);
+
+    float len = length(lightDirection);
+    lightDirection /= len;
+    float lightAtten = min(1. / (.25 * len * len), 1.);
+
+    vec3 ref = reflect(-lightDirection, surfNormal);
+
+    float ambientOcclusion = 1.;
+
+    float ambient = .15;
+    float specularPower = 128.;
+    float diffuse = max(0., dot(surfNormal, lightDirection));
+    float specular = max(0., dot(ref, normalize(camPos-surfacePosition)));
+    specular = pow(specular, specularPower);
+
+    sceneColor += (objColor*(diffuse*.8+ambient)+specular*.5)*lightColor*lightAtten*ambientOcclusion;
+
+    return clamp(sceneColor, 0., 1.);
+}
+
 void main() {
   vec2 aspect = vec2(u_resolution.x / u_resolution.y, 1.);
 	vec2 screenCoords = (2. * gl_FragCoord.xy / u_resolution.xy - 1.) * aspect;
 
-	vec3 lookAt = vec3(0.,0.,0.);
+	vec3 lookAt = vec3(0., 1. * sin(u_time * .5), u_time);
 	vec3 camPos = vec3(1.0*sin(u_time*0.5), 0.15*sin(u_time*0.25), 1.0*cos(u_time*0.5)+u_time);
 
   vec3 forward = normalize(lookAt - camPos);
@@ -65,44 +101,15 @@ void main() {
   vec3 rayOrigin = camPos;
   vec3 rayDirection = normalize(forward + FOV * screenCoords.x * right + FOV * screenCoords.y * up);
 
-  vec3 bgcolor = vec3(.01);
-
-	const float clipNear = 0.;
-	const float clipFar = 8.;
-	float dist = rayMarching(rayOrigin, rayDirection, clipNear, clipFar);
+	const float clipFar = 8.0;
+	float dist = rayMarching(rayOrigin, rayDirection, 0., clipFar);
 
 	if (dist >= clipFar) {
-    gl_FragColor = vec4(bgcolor, 1.0);
+    gl_FragColor = vec4(vec3(.01), 1.0);
     return;
 	}
 
 	vec3 surfacePosition = rayOrigin + rayDirection * dist;
 
-	vec3 surfNormal = getNormal(surfacePosition);
-
-	vec3 lightPos = vec3(1.0*sin(u_time*0.5), 0.15*sin(u_time*0.25), 1.0*cos(u_time*0.5)+u_time -4.);
-	vec3 lightDirection = lightPos - surfacePosition;
-	vec3 lcolor = vec3(.95, .97, .99);
-
-	float lightLen = length(lightDirection);
-	lightDirection /= lightLen;
-	float lightAtten = min(1. / (.25 * lightLen * lightLen), 1.);
-
-	vec3 ref = reflect(-lightDirection, surfNormal);
-
-	vec3 sceneColor = vec3(0.);
-
-	vec3 objColor = vec3(.3);
-	float bumps = sinusoidBumps(surfacePosition);
-  objColor = clamp(objColor * .8 - vec3(.1, .4, .5) * bumps, 0., 1.);
-
-	float ambient = .5;
-	float specularPower = 8.;
-	float diffuse = max(0., dot(surfNormal, lightDirection));
-	float specular = max(0., dot(ref, normalize(camPos - surfacePosition)));
-	specular = pow(specular, specularPower);
-
-	sceneColor += (objColor*(diffuse*0.8+ambient)+specular*0.5)*lcolor*lightAtten;
-
-	gl_FragColor = vec4(clamp(sceneColor, 0., 1.), 1.);
+	gl_FragColor = vec4(lighting(surfacePosition, camPos, 0), 1.);
 }
